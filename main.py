5141f3ee96bd4d36cf6657431adff4e7c9c022c9
Nickhil215 <nickhil215@gmail.com>
2025-03-10 12:47:48 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+ 
+# Auto-generated Python Script
+
+# Required Imports
+from flask import Request
+
+import requests.sessions
+
+
+# Function Definitions
+def hello_world3(request: Request):
+    """Function to handle API requests"""
+
+    # Parse the JSON body
+    request_json = request.get_json(silent=True)
+
+    # Extract required parameters dynamically
+    
+    url = request_json.get("url")
+    
+    params = request_json.get("params")
+    
+
+    # Make API request dynamically
+    response = requests.api.get(url, params)
+
+    # Return response as json
+    return response.json()
